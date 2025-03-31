38efc36999181dcddac0c39a4fc8e400d2e24944
rootdobos <49080519+rootdobos@users.noreply.github.com>
2025-03-31 23:44:09 +0300

    added tile extraction endpoint

--- backend/views/tiles.py
+++ backend/views/tiles.py
@@ -9,13 +9,13 @@ load_dotenv()
 
 def extract_tiles_from_slide(request):
     id = request.GET.get("slideId")  # Get the 'param' value from the URL
-    slide_dir=os.getenv("TILES_FOLDER")
-    output_dir= os.getenv("")
+    slide_dir = os.getenv("SLIDES_FOLDER")
+    output_dir = os.getenv("TILES_FOLDER")
     if not id:
         return JsonResponse({"error": "Missing slide id"}, status=400)
-    
+
     try:
-        TileService.extract_tiles(slide_dir,output_dir)
-        return JsonResponse({"message": f"Received parameter: {id}"})
+        TileService.extract_tiles(id, slide_dir, output_dir)
+        return JsonResponse({"message": f"Extracted tiles from: {id}"})
     except Exception:
         return JsonResponse({"error": "Error in extraction"}, status=400)
